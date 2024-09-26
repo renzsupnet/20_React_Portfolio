a2b34fb38eba815e4330f82184a67f31d83a5200
Renz Carl Supnet <31431868+renzsupnet@users.noreply.github.com>
2024-09-26 02:42:48 -0700

    Updated portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -23,7 +23,7 @@ const Portfolio = ({ project, style }) => {
         <img src={project.image} className="card-img-top" alt={project.title} style={imageStyle}/>
         <div className="card-body" style={cardBody}>
           <h5 className="card-title">{project.title}</h5>
-          <p className="card-text">{project.description}</p>
+          <p className="card-text">{project.description} (Links will take a few min to load)</p>
           <a href={project.github} className="btn btn-primary" style={iconStyle}><FaGithub/></a>
           <a href={project.demo} className="btn btn-primary" style={iconStyle}><FaFirefoxBrowser/></a>
         </div>
